4ebdeb22a63a6d7623e593a49ca96da8b5545fda
gotenkusu48@yahoo.co.jp <gotenkusu48@yahoo.co.jp>
2015-01-06 21:04:08 +0900

    chabgeCode

--- TypeScriptHTMLApp2/SlimeHit2D/SlimeHit2D.ts
+++ TypeScriptHTMLApp2/SlimeHit2D/SlimeHit2D.ts
@@ -1,21 +1,15 @@
-﻿module SlimeHit2D {
-    // 二次元ベクトルのクラス
-    export class Vector {
-        // コンストラクター
-        constructor(public x: number = 0.0, public y: number = 0.0) { }
+﻿
+module SlimeHit2D {
+    class SlimeHit2D {
+        HitCheck(x: Vecter.Vector[], y: Vecter.Vector[]): void{
+            var ary = new Array;
+            for(var i = 0; i < y.length;i++){
+                //Xの配列のどれにアクセスするか
+                var x_x = x[i].x;
+                var x_y = x[i].y;
+                
 
-        // プロパティ (C# のプロパティにあたる/型付き)
-        get length(): number {
-            return Math.sqrt(this.x * this.x + this.y * this.y);
         }
-
-        // メソッド (引数、戻り値ともに型付きにできる)
-        add(vector: Vector): Vector {
-            return new Vector(this.x + vector.x, this.y + vector.y);
         }
-
-        //multiply(vector: Vector): Vector {
-        //    return new Vector(this.x * vector.x, this.y * vector.y);
-        //}
     }
 }
---
+++ TypeScriptHTMLApp2/SlimeHit2D/Vecter.ts
@@ -0,0 +1,35 @@
+﻿module Vecter {
+    // 二次元ベクトルのクラス
+    export class Vector {
+        // コンストラクター
+        constructor(public x: number = 0.0, public y: number = 0.0) { }
+
+        // プロパティ (C# のプロパティにあたる/型付き)
+        get length(): number {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        }
+
+        // メソッド (引数、戻り値ともに型付きにできる)
+        add(vector: Vector): Vector {
+            return new Vector(this.x + vector.x, this.y + vector.y);
+        }
+
+        //multiply(vector: Vector): Vector {
+        //    return new Vector(this.x * vector.x, this.y * vector.y);
+        //}
+    }
+    export class Vector2D {
+        // コンストラクタ
+        constructor(public x: number = 0.0,public x2:number = 0.0, public y: number = 0.0,public y2 =0.0) { }
+
+        // プロパティ (C# のプロパティにあたる/型付き)
+        get length(): number {
+            return Math.sqrt(this.x * this.x + this.x2 * this.x2 + this.y * this.y);
+        }
+
+        // メソッド (引数、戻り値ともに型付きにできる)
+        add(vector: Vector): Vector {
+            return new Vector(this.x + vector.x, this.y + vector.y);
+        }
+    }
+}
