27927da2163e917fc933912992a81dd5d23f4e01
gotenkusu48@yahoo.co.jp <gotenkusu48@yahoo.co.jp>
2015-01-08 23:35:14 +0900

    codechange

---
+++ TypeScriptHTMLApp2/SlimeHit2D/SlimeHit.ts
@@ -0,0 +1,19 @@
+﻿module SlimeHit {
+    class SlimeHit {
+        HitCheckPoint2Point(x: Vecter.Vector[], y: Vecter.Vector[]): void {
+            var ary = new Array;
+            for (var i = 0; i < y.length; i++) {
+                //Xの配列のどれにアクセスするか
+                var x_x:number = x[i].x;//XのX座標
+                var x_y: number = x[i].y;//XのY座標
+                var y_x: number = y[i].x;//YのX座標
+                var y_x: number = y[i].y;//YのY座標
+
+                var result = new Array;
+                if (x_x == y_x & x_y == y_x) {
+                }
+
+            }
+        }
+    }
+}
